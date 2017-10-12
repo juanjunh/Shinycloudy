6cab5accccbe94ff2e2fe19f5397a7fc0dcbb1ea
JanielHNY <janielhny@JanielHNYde-MacBook-Air.local>
2017-10-12 20:55:09 +0930

    first commit, interface not completed, alamofire and cocoapods installed unsuccessfully

---
+++ Shinycloudy/WeatherVC.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherVC.swift
+//  Shinycloudy
+//
+//  Created by JanielHNY on 2017/10/12.
+//  Copyright © 2017年 JanielHNY. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+
+}
+
