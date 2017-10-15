1f4e8938de158b51621f5bdbf3a58b286a504b5c
JanielHNY <janielhny@JanielHNYde-MacBook-Air.local>
2017-10-15 12:00:52 +0930

    second commit. interface, delegate and datasource completed

--- Shinycloudy/WeatherVC.swift
+++ Shinycloudy/WeatherVC.swift
@@ -8,12 +8,45 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var dateLbl: UILabel!
+    
+    @IBOutlet weak var currentTempLbl: UILabel!
+    
+    @IBOutlet weak var locationLbl: UILabel!
+    
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    
+    @IBOutlet weak var weatherTypeLbl: UILabel!
+    
+    @IBOutlet weak var weatherTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        weatherTableView.delegate = self
+        weatherTableView.dataSource = self
     }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = weatherTableView.dequeueReusableCellWithIdentifier("weatherCell", forIndexPath: indexPath)
+        return cell
+    
+    }
+    
+    
+    
+    
+    
 
     
 
